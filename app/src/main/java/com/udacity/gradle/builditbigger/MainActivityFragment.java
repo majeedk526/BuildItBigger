192e9886089d360b99c156606dc305bbd5c5674d
Majeed <majeedk526@gmail.com>
2016-08-11 00:41:50 -0800

    refactored code

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,25 +1,16 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.jokes.Joke;
-import com.portfolio.majeed.andjokelib.JokeActivity;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment implements View.OnClickListener {
+public class MainActivityFragment extends Fragment {
 
-    Button btnTellJoke;
 
     public MainActivityFragment() {
     }
@@ -30,29 +21,8 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
 
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
 
-        btnTellJoke = (Button) root.findViewById(R.id.btn_tell_joke);
-        btnTellJoke.setOnClickListener(this);
 
         return root;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-
-            case R.id.btn_tell_joke:
-
-                break;
-
-        }
     }
 }
